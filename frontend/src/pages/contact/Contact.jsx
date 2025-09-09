2fbd1f4ef80084b35e919b42275fc61558ca0da5
imran2454 <mi3582301@gmail.com>
2025-09-09 11:13:49 -0700

    addded responsive addmition

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -31,7 +31,7 @@ function ContactForm() {
    <>
    <ContactDetails/>
    <GoogleMapEmbed/>
-    <form onSubmit={handleSubmit} className="max-w-lg mx-auto py-6 bg-white shadow-md rounded-lg mt-10 ">
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg mt-10 ">
       <h2 className="text-xl font-bold mb-4">Contact Us</h2>
       <label className="text-xl">Enter Your Name</label>
       <input
